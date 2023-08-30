2085ba4a411f6ac2a53a6d4838a322b414687c14
YCStarburst <yusuf.cattaneo@starburstdata.com>
2023-08-30 18:37:12 +0000

    Implementing SCD Type 4

--- models/Postgres_datalake/hive_customer.sql
+++ models/Postgres_datalake/hive_customer.sql
@@ -1,12 +1,35 @@
 {{
     config(
-        materialized='incremental',
-        unique_key='custkey',
+        materialized="incremental",
+        unique_key="custkey",
+        properties= {
+            "format": "'PARQUET'",
+            "type": "'HIVE'"
+        }
     )
 }}
 
-with postgres_customer_ as (
-     select * from {{ source('demo_postgres_aws', 'customer')}}
+with
+    postgres_customer_ as (
+        select *
+        from {{ source("demo_postgres_aws", "customer") }}
+    ),
+
+add_timestamp as (
+ select *, 
+    cast(current_timestamp as varchar)  ---timestamp(3) is not supported in hive
+                                        as last_updated_time
+    from postgres_customer_
+),
+
+new_rows as (
+    select * from add_timestamp 
+    {% if is_incremental() %}
+      where last_updated_time > (
+        select max(last_updated_time)
+        from {{ this }}
+      )
+    {% endif %}
 )
- 
-select * from postgres_customer_
+
+select * from new_rows
--- models/Postgres_datalake/iceberg_customer.sql
+++ models/Postgres_datalake/iceberg_customer.sql
@@ -1,37 +1,53 @@
 {{
     config(
-        materialized='incremental',
-        unique_key='custkey',
-        properties= {
+        materialized="incremental",
+        unique_key="custkey",
+        properties={
             "format": "'PARQUET'",
             "type": "'ICEBERG'",
             "partitioning": "ARRAY['month(registration_date)']",
-            "sorted_by": "ARRAY['custkey']"
-        }
+            "sorted_by": "ARRAY['custkey']",
+        },
     )
 }}
 
-with postgres_hive_customer_ as (
-     select
-  custkey,
-  first_name,
-  last_name,
-  cast(registration_date as date) as registration_date,
-  street,
-  city,
-  state,
-  postcode,
-  country,
-  phone,
-  dob,
-  gender,
-  married,
-  ssn,
-  paycheck_dd,
-  estimated_income,
-  fico
-    from {{ ref('hive_customer')}}
-    where registration_date > '1999-12-31'
-)
+with
+    dedup_hive as (
+        select *
+        from
+            (
+                select
+                    *,
+                    row_number() over (
+                        partition by custkey order by last_updated_time desc
+                    ) as row_num
+                from {{ ref("hive_customer") }}
+                where registration_date > '1999-12-31'
+            )
+        where row_num = 1
+    ),
 
-select * from postgres_hive_customer_
+    postgres_iceberg_customer_ as (
+        select
+            custkey,
+            first_name,
+            last_name,
+            cast(registration_date as date) as registration_date,
+            street,
+            city,
+            state,
+            postcode,
+            country,
+            phone,
+            dob,
+            gender,
+            married,
+            ssn,
+            paycheck_dd,
+            estimated_income,
+            fico
+        from dedup_hive
+    )
+
+select *
+from postgres_iceberg_customer_
