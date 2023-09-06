7d187ed703f2fc5acadc851f54875f446dbf8e11
YCStarburst <yusuf.cattaneo@starburstdata.com>
2023-09-06 20:18:37 +0000

    Leverages Snapshot

---
+++ snapshots/postgres_burst_bank_customer_snapshot.sql
@@ -0,0 +1,37 @@
+{% snapshot postgres_burst_bank_customer_snapshot %}
+
+{{
+    config(
+      target_database='demo_aws_s3',
+      target_schema='dbt_snapshots',
+      unique_key='custkey',
+      strategy='check',
+      check_cols='all',
+      properties={
+            "format": "'PARQUET'",
+            "type": "'ICEBERG'"
+        },
+    )
+}}
+
+select custkey,
+    first_name,
+    last_name,
+    cast(registration_date as timestamp(6) with time zone) as registration_date,
+    street,
+    city,
+    state,
+    postcode,
+    country,
+    phone,
+    dob,
+    gender,
+    married,
+    ssn,
+    paycheck_dd,
+    estimated_income,
+    fico
+    from {{ source("demo_postgres_aws", "customer") }}
+
+{% endsnapshot %}
+
